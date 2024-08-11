7313260a350d6e74b86e8a60c63e615ed0cc38d8
Doris Andrea Valverde Jara <andrea.avj03@gmail.com>
2024-08-11 16:58:52 -0400

    js

--- assets/js/scrits.js
+++
@@ -1,8 +0,0 @@
-function pintar(element, color = "green") {
-    element.style.backgroundColor = color;
-  }
-  
-  const ele = document.getElementById("ele1");
-  ele.addEventListener("click", function () {
-    pintar(ele, "yellow");
-  });
